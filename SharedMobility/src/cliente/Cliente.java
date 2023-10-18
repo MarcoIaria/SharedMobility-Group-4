4e858a39f17c5cc460d65be20ba429950c78ad33
MarcoIaria <marco.iaria71@gmail.com>
2023-10-18 17:21:44 +0200

    Client Class

--- SharedMobility/src/cliente/Cliente.java
+++ SharedMobility/src/cliente/Cliente.java
@@ -1,5 +1,28 @@
 package cliente;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String dataDiNascita;
+    private String codiceFiscale;
+    private List<String> patenti;
+    //Srtring patenti;
+
+    private boolean haCasco;
+    private double credito;
+
+    public Cliente(int id, String nome, String cognome, String dataDiNascita, String codiceFiscale) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataDiNascita = dataDiNascita;
+        this.codiceFiscale = codiceFiscale;
+        this.patenti = new ArrayList<>();
+        this.haCasco = false;
+        this.credito = 0.0;
+    }
 }
